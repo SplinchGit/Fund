5f4cdecc8b79a0ddd0534966d873ec6a19139f59
James <jf358950@gmail.com>
2025-04-26 06:19:18 +0100

    Amplify BS-ery more

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,18 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // This is crucial for Amplify
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
