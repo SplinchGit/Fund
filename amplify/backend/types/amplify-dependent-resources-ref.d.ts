13792dc904c067bfcac68907b0012d05e527c1eb
James <jf358950@gmail.com>
2025-05-13 07:07:50 +0100

    Ugh

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "WorldFundAPI": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
   "auth": {
     "0worldfunddebugd1266f95": {
       "AppClientID": "string",
