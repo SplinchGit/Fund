a013151960264ff2546bf30bfbc9851602355fe1
James <jf358950@gmail.com>
2025-06-28 00:52:24 +0100

    Dependency updates.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  VITE_AMPLIFY_API: any;
   readonly VITE_WORLD_APP_ID: string;
   readonly VITE_WORLD_ACTION_ID: string;
   readonly VITE_DATABASE_URL: string;
