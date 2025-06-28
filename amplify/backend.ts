d3729919d127e8b387aeb6c435543a2c759d48c7
James <jf358950@gmail.com>
2025-06-28 04:44:23 +0100

    Losing my mind.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
 
 /**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ * Minimal backend - just enough for Amplify to deploy frontend
+ * Uses existing backend infrastructure
  */
-defineBackend({
-  auth,
-  data,
+export const backend = defineBackend({
+  // Empty - we don't want Amplify managing our existing resources
 });
