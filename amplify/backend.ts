fce2dff3092375e8ce3f30b17da6f66dda64bb73
James <jf358950@gmail.com>
2025-06-29 23:29:27 +0100

    Add S3 storage for campaign image uploads

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
 import { storage } from './storage/resource';
 
 /**
- * Backend with S3 storage for campaign images 
+ * Backend with auth and S3 storage for campaign images 
  */
 export const backend = defineBackend({
+  auth,
   storage
 });
