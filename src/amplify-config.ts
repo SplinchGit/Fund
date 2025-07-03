01779a51545669aacf3605cc27540dab08c39de4
James <jf358950@gmail.com>
2025-07-03 18:38:20 +0100

    Multiple updates.

--- src/amplify-config.ts
+++ src/amplify-config.ts
@@ -2,25 +2,15 @@
 import { Amplify } from '@aws-amplify/core';
 import { generateClient } from '@aws-amplify/api';
 
-// Amplify Gen 1 configuration with REAL values from aws-exports.js
+// Amplify Gen 1 configuration with proper storage setup
 const amplifyConfig = {
   aws_project_region: 'eu-west-2',
   aws_cognito_region: 'eu-west-2',
-  aws_user_pools_id: 'eu-west-2_EZv70F47u',
-  aws_user_pools_web_client_id: '57lvledp56fqp7sohrb1l8djai',
-  aws_cognito_identity_pool_id: 'eu-west-2:4c836def-a749-4f5f-b228-51dc33474129',
+  aws_user_pools_id: 'eu-west-2_placeholder',
+  aws_user_pools_web_client_id: 'placeholder',
+  aws_cognito_identity_pool_id: 'placeholder',
   aws_user_files_s3_bucket: 'fundbucketimages0017bb3c-dev',
   aws_user_files_s3_bucket_region: 'eu-west-2',
-  aws_cognito_username_attributes: [],
-  aws_cognito_social_providers: [],
-  aws_cognito_signup_attributes: ['EMAIL'],
-  aws_cognito_mfa_configuration: 'OFF',
-  aws_cognito_mfa_types: ['SMS'],
-  aws_cognito_password_protection_settings: {
-    passwordPolicyMinLength: 8,
-    passwordPolicyCharacters: []
-  },
-  aws_cognito_verification_mechanisms: ['EMAIL'],
   Storage: {
     AWSS3: {
       bucket: 'fundbucketimages0017bb3c-dev',
@@ -36,10 +26,9 @@ Amplify.configure(amplifyConfig);
 export const client = generateClient();
 
 console.log('[amplify-config] Amplify configured with fallback config:', {
-  auth: !!amplifyConfig.auth,
-  storage: !!amplifyConfig.storage,
+  storage: !!amplifyConfig.Storage,
   region: amplifyConfig.aws_project_region,
-  bucket: amplifyConfig.storage.bucket_name
+  bucket: amplifyConfig.Storage.AWSS3.bucket
 });
 
 export default amplifyConfig;
